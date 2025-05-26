da1068271471e39150038509ba83d41bf9b13620
id9ard <brossard.louis96@gmail.com>
2025-05-26 16:17:46 -0300

    Header- Falta funcionalidad del audio

---
+++ index/index.js
@@ -0,0 +1,31 @@
+// Imagenes del slider
+const images = [
+    "../img/safari4.jpg",
+    "../img/safari5.jpg",
+    "../img/safari3.png",
+];
+
+const audio_node = document.querySelector(".navR_content-left");
+
+// Cambiar las imagenes dek slider con la de la variable "images"
+let imagePosition = 0;
+function changeImage() {
+    document.querySelector(".header").style.backgroundImage = `url(${images[imagePosition]})`;
+    if (imagePosition < images.length - 1) imagePosition++;
+    else imagePosition = 0;
+    setTimeout("changeImage()", 3000);
+}
+
+// Cambiar el icono de volumen con cada "click"
+function changeAudio() {
+    audio_node.addEventListener("click", () => {
+        if (audio_node.value == "volx") audio_node.value = "vol";
+        else audio_node.value = "volx";
+        audio_node.innerHTML = `
+            <img class="audio" src="../img/${audio_node.value}.svg" alt="audio">`
+    });
+}
+
+
+changeImage();
+changeAudio();
