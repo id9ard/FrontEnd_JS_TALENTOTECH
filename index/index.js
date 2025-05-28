04ee3653ace4d5cf9ae7bc1189723a561de8047e
id9ard <brossard.louis96@gmail.com>
2025-05-28 01:50:29 -0300

    Menu funcional - falta linkear los enlaces

--- index/index.js
+++ index/index.js
@@ -6,6 +6,7 @@ const images = [
 ];
 
 const audio_node = document.querySelector(".navR_content-left");
+const menu_node = document.querySelector(".nav-left");
 
 // Cambiar las imagenes dek slider con la de la variable "images"
 let imagePosition = 0;
@@ -21,9 +22,11 @@ let audio = new Audio("./audio/audio.mp3");
 function changeAudio() {
     audio_node.addEventListener("click", () => {
         if (audio_node.value == "volx") {
+            //Reproducir audio
             audio.play();
             audio_node.value = "vol";
         } else {
+            //Pausar audio
             audio.pause();
             audio_node.value = "volx";
         }
@@ -32,6 +35,24 @@ function changeAudio() {
     });
 }
 
+// Cambiar el icono de menu con cada "click"
+function ChangeMenu() {
+    menu_node.addEventListener("click", () => {
+        if (menu_node.value == "open") {
+            //Abrir menu
+            document.querySelector(".header_menu").style.display = "flex";
+            menu_node.value = "close";
+        } else {
+            //Cerrar menu
+            document.querySelector(".header_menu").style.display = "none";
+            menu_node.value = "open";
+        }
+        menu_node.innerHTML = `
+            <img class="menu" src="./img/${menu_node.value}.svg" alt="">`
+    })
+}
+
 
 changeImage();
 changeAudio();
+ChangeMenu();
