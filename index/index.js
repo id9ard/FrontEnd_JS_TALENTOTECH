fa1a1ad95e6260b3e367887d439f498b779441fd
id9ard <brossard.louis96@gmail.com>
2025-06-02 22:52:53 -0300

    formulario y cambios en  el js

--- index/index.js
+++ index/index.js
@@ -7,27 +7,30 @@ const images = [
 
 const audio_node = document.querySelector(".navR_content-left");
 const menu_node = document.querySelector(".nav-left");
+const section_form_node = document.querySelector("#section_form");
+const item_form_node = document.querySelector("#item_form");
+const boton_close_node = document.querySelector(".four_button-close");
 
 // Cambiar las imagenes dek slider con la de la variable "images"
 let imagePosition = 0;
-function changeImage() {
+function slider() {
     document.querySelector(".header").style.backgroundImage = `url(${images[imagePosition]})`;
     if (imagePosition < images.length - 1) imagePosition++;
     else imagePosition = 0;
-    setTimeout("changeImage()", 3000);
+    setTimeout("slider()", 3000);
 }
 
 // Cambiar el icono de volumen con cada "click"
-let audio = new Audio("./audio/audio.mp3");
-function changeAudio() {
+let audioURL = new Audio("./audio/audio.mp3");
+function audio() {
     audio_node.addEventListener("click", () => {
         if (audio_node.value == "volx") {
             //Reproducir audio
-            audio.play();
+            audioURL.play();
             audio_node.value = "vol";
         } else {
             //Pausar audio
-            audio.pause();
+            audioURL.pause();
             audio_node.value = "volx";
         }
         audio_node.innerHTML = `
@@ -36,16 +39,18 @@ function changeAudio() {
 }
 
 // Cambiar el icono de menu con cada "click"
-function ChangeMenu() {
+function menu() {
     menu_node.addEventListener("click", () => {
         if (menu_node.value == "open") {
             //Abrir menu
+            document.body.style.overflow = "hidden";
             document.querySelector(".header_menu").style.display = "flex";
             menu_node.value = "close";
             // Cerrar el menu si se hace click en alguna opcion
-            changeSection();
+            selectSection();
         } else {
             //Cerrar menu
+            document.body.style.overflow = "revert";
             document.querySelector(".header_menu").style.display = "none";
             menu_node.value = "open";
         }
@@ -54,18 +59,38 @@ function ChangeMenu() {
     })
 }
 
-function changeSection() {
+function selectSection() {
     const section_node = document.querySelectorAll(".menu_text");
     section_node.forEach(element => {
         element.addEventListener("click", () => {
+            document.body.style.overflow = "revert";
             document.querySelector(".header_menu").style.display = "none";
             menu_node.value = "open";
             menu_node.innerHTML = `
             <img class="menu" src="./img/${menu_node.value}.svg" alt="">`
+
+            section_form_node.style.display = "none";
         })
     });
 }
 
-changeImage();
-changeAudio();
-ChangeMenu();
+function openForm() {
+    item_form_node.addEventListener("click", () => {
+        section_form_node.style.display = "flex";
+        document.body.style.overflow = "hidden";
+    });
+}
+
+function closeForm() {
+    boton_close_node.addEventListener("click", () => {
+        section_form_node.style.display = "none";
+        document.body.style.overflow = "revert";
+    });
+}
+
+slider();
+audio();
+menu();
+
+openForm();
+closeForm();
